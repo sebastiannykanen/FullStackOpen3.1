fc6df9181f3dba2aab959a4a98b95f6e0f8a5761
Sebastian Nykänen <sebastian.nykanen@outlook.com>
2023-02-23 11:29:02 +0200

    edited mongo.js

--- mongo.js
+++ mongo.js
@@ -24,7 +24,14 @@ const note = new Note({
   important: true,
 });
 
-note.save().then((result) => {
-  console.log("note saved!");
+// note.save().then((result) => {
+//   console.log("note saved!");
+//   mongoose.connection.close();
+// });
+
+Note.find({}).then((result) => {
+  result.forEach((note) => {
+    console.log(note);
+  });
   mongoose.connection.close();
 });
